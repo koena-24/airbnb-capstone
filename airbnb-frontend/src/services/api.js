4107c266056ad2fce4aae58c382264a711a3fdc5
koena <dr.koena@gmaile.com>
2026-07-02 19:23:08 +0200

    Update API URLs for production deployment

--- airbnb-frontend/src/services/api.js
+++ airbnb-frontend/src/services/api.js
@@ -1,17 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("userToken");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
+  baseURL: "https://airbnb-capstone-mkdq.onrender.com/api",
 });
 
 export default api;
